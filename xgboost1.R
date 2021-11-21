ea6298edc0f2a07420e00ce0671f7d4c694b3722
Akeem <akeembyusuf@gmail.com>
2021-11-20 22:22:12 -0700

    Update xgboost1.R

--- xgboost1.R
+++ xgboost1.R
@@ -10,37 +10,55 @@ table(mytrain$AsthmaStatus)
 which(colnames(mytrain) == "AsthmaStatus")
 
 # Create new dataset
-trainingset <-  mytrain[c(-509)]
-dim(trainingset)
-head(trainingset)
+traindata <-  mytrain[c(-509,-7,-8)]
+head(traindata)
+summary(traindata)
+dim(traindata)
+str(traindata)
+is.data.frame(traindata)
+table(traindata$AsthmaStatus)
+which(colnames(traindata) == "AsthmaStatus")
 
 
 # Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
+
+#install.packages('caTools')
 library(caTools)
 set.seed(2021)
-split = sample.split(trainingset$AsthmaStatus, SplitRatio = 0.8)
-training_set = subset(trainingset, split == TRUE)
-test_set = subset(trainingset, split == FALSE)
+split = sample.split(traindata$AsthmaStatus, SplitRatio = 0.80)
+train_set = subset(traindata, split == TRUE)
+test_set = subset(traindata, split == FALSE)
 
 # Fitting XGBoost to the Training set
-# install.packages('xgboost')
+
+#install.packages('xgboost')
 library(xgboost)
-classifier = xgboost(data = as.matrix(training_set[-6]), label = training_set$AsthmaStatus, nrounds = 20)
+classifier = xgboost(data = as.matrix(train_set[-5]),
+                     label = train_set$AsthmaStatus, 
+                     nrounds = 10, objective = 'binary:logistic')
+
+
+#Important featture
+importance_matrix <- xgb.importance(model = classifier)
+print(importance_matrix$Gain)
+print(importance_matrix$Importance)
+print(importance_matrix$Frequency)
+xgb.plot.importance(importance_matrix = importance_matrix)
+
+#Trees from XGBoost
+xgb.plot.tree(model = classifier)
+
 
 # Predicting the Test set results
-y_pred = predict(classifier, newdata = as.matrix(test_set[-6]))
+y_pred = predict(classifier, newdata = as.matrix(test_set[-5]))
 y_pred = (y_pred >= 0.5)
 
-y# Making the Confusion Matrix
-cm = table(test_set[, -6], y_pred)
+# Making the Confusion Matrix
+cm = table(test_set[, -5], y_pred)
 
-mean((test_set[,6] - y_pred)^2) #mse
-caret::MAE(test_set[,6], y_pred) #mae
-caret::RMSE(test_set[,6], y_pred) #rmse
 
-mean(test_set[,6] == y_pred)
-mean(test_set[,6] != y_pred)
+mean(test_set[,5] == y_pred) #0.8649131
+mean(test_set[,5] != y_pred) #0.1350869
 
 table(y_pred)
 # Applying k-Fold Cross Validation
@@ -48,19 +66,23 @@ table(y_pred)
 #install.packages('dplyr')
 
 library(caret)
-folds = createFolds(training_set$AsthmaStatus, k = 10)
+library(dplyr)
+
+folds = createFolds(train_set$AsthmaStatus, k = 10)
 cv = lapply(folds, function(x) {
-  training_fold = training_set[-x, ]
-  test_fold = training_set[x, ]
-  classifier = xgboost(data = as.matrix(training_set[-6]), 
-                       label = training_set$AsthmaStatus, nrounds = 10)
-  y_pred = predict(classifier, newdata = as.matrix(test_fold[-6]))
+  training_fold = train_set[-x, ]
+  test_fold = train_set[x, ]
+  classifier = xgboost(data = as.matrix(train_set[-5]), 
+                       label = train_set$AsthmaStatus, 
+                       nrounds = 20)
+  y_pred = predict(classifier, newdata = as.matrix(test_fold[-5]))
   y_pred =(y_pred >= 0.5)
-  cm = table(as.matrix(test_fold[, 6]), y_pred)
+  cm = table(as.matrix(test_fold[, 5]), y_pred)
   accuracy = (cm[1,1] + cm[2,2]) / (cm[1,1] + cm[2,2] + cm[1,2] + cm[2,1])
   return(accuracy)
 })
 accuracy = mean(as.numeric(cv))
 accuracy
 
+#87.09 Accurracy
 
